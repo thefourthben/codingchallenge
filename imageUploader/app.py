f1827ec4514d12be7a2564b895c4238b85f7c8df
Wei-Min Chou <chouweimin@berkeley.edu>
2021-01-29 03:52:10 +0800

    sdf

--- imageUploader/app.py
+++ imageUploader/app.py
@@ -1,11 +1,11 @@
 # Code based on pythonbuddy.com
 
-from flask import Flask, render_template, request, jsonify, session
+from flask import Flask, render_template, request, jsonify, session, flash
 
 # Configure Flask App
 # Remember to change the SECRET_KEY!
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.config['SECRET_KEY'] = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
 app.config['DEBUG'] = True
 
 @app.route('/')
@@ -19,15 +19,71 @@ def index():
 '''
 BEGIN TASK 1
 '''
-# Should you use a GET or POST Request?
-# @app.route('/submit_image', methods=['POST'])
+counts = {}
+
+# Should you use a GET or POST Request? POST
+@app.route('/submit_image', methods=['POST'])
 # @app.route('/submit_image', methods=['GET'])
+
 def submit_image():
     """Adds an image to our server and adds it to our webpage. 
         Automatically refreshes our webpage so that user can see the image appended
     """
-    # How do we store images in our server?
+    # Setting up the necessary variables that will be used
+    if session["count"] == 0:
+        session["images"] = []
+        session["count"] += 1
+    
+    # checking that the files exist
+    if request.files:
 
+        # getting the image that will be uploaded
+        image = request.files["image_to_be_uploaded"]
+
+        # getting the size of the image
+        length = request.content_length
+
+        # Checking that there is an uploaded file
+        if image.filename != '' and length != 0:
+
+            # Checking that the filename has no spaces            
+            if " " not in image.filename:
+                img = image.filename
+
+                # Checking the upload size
+                if length > 20 * 1024 * 1024:
+
+                    # If the current title exist
+                    if img in session["images"]:
+                        counts[img] += 1
+                        img = image.filename + "("+ str(counts[img]) +")"
+                    else:
+                        counts[img] = 0
+
+                    # Saving the image on the servere side
+                    image.save("static/" + img)
+
+                    # updating the session variables
+                    curr = session["images"]
+                    curr.append(img)
+                    session["images"] = curr
+                    return render_template('index.html', files = session["images"])
+
+                # Showing user a message to ask to lower the upload size
+                else:
+                    flash("Please don't upload an image more than 20MB")
+                    return render_template('index.html', files = session["images"])
+
+            # Showing user a message to ask to change the filename
+            else:
+                flash("Please remove spaces from your filename!")
+                return render_template('index.html', files = session["images"])
+
+        # If there isn't an uploaded photo, then display messagee to users
+        else:
+            flash("Please select a photo before you submit!")
+            return render_template('index.html', files = session["images"])
+            
 '''
 END TASK 1
 '''
--- imageUploader/static/js/script.js
+++ imageUploader/static/js/script.js
@@ -1,28 +1,19 @@
 $(document).ready(function() {
     /* BEGIN TASK 1 */
 
-    /* HINT 1:
-        Look into event listeners: https://api.jquery.com/category/events/
-    /*
-    /* Example of a hover listener: */
-    // $("#submission").hover(function() {
-    //     alert("Form submission button clicked");
-    // });
-
-    /* HINT 2:
-        After you've created an event listener, how do you make a call to your 
-        API endpoint /submit_image with the image data w/o refreshing the page?
-
-        Look into AJAX calls in jQuery: https://api.jquery.com/jquery.ajax/
-    /* 
-    /* Example of an AJAX request */
-    // .post('/submit_image', {
-    //     text: "hello world"
-    //   }, function(data) {
-    //     console.log("Just made a POST Request :)");
-    //     call_a_function(current_text);
-    //     return false;
-    //   }, 'json');
-
+    // When the submit button is clicked execute the function below
+    $("#submission").click(function(){
+        // getting the data and using ajax to send data
+        var formdata = new FormData($("#form")[0]);
+        $.ajax({
+            type: "POST",
+            url : "/submit_image",
+            data: formdata,
+            dataType: "json",
+            success: function(){
+                document.location.href = "http://127.0.0.1:5000/submit_image"
+            }
+        });
+    });
     /* END TASK 1 */
 })
